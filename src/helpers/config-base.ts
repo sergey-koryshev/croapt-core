0568b7ef9a53ea0849f0b635bfa5b90ea80848e5
Sergey Koryshev <sergey.koryshev@outlook.com>
2026-03-03 08:55:09 +0100

    Added ConfigBase

---
+++ src/helpers/config-base.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs'
+
+/**
+ * Base class for configuration management. It loads configuration and secrets from JSON files based on the specified environment.
+ * The configuration files should be named in the format 'config.{environment}.json' and 'secrets.{environment}.json'.
+ * If the environment is not specified or is 'production', it will look for 'config.json' and 'secrets.json'.
+ */
+export abstract class ConfigBase<T extends ConfigBase<T>> {
+  /**
+   * The loaded configuration object of type T.
+   */
+  protected readonly config: Partial<T>
+
+  /**
+   * The loaded secrets object of type T.
+   */
+  protected readonly secrets: Partial<T>
+
+  constructor(environment?: string) {
+    const configSuffix = environment == null || environment === '' || environment.toLowerCase() === 'production'
+      ? ''
+      : `.${environment}`
+    const fullConfigName = `config${configSuffix}.json`
+    const fullSecretsName = `secrets${configSuffix}.json`
+
+    if (!fs.existsSync(fullConfigName)) {
+      throw new Error(`Configuration file '${fullConfigName}' cannot be found.`)
+    }
+
+    this.secrets = {} as Partial<T>
+    if (fs.existsSync(fullSecretsName)) {
+      this.secrets = JSON.parse(fs.readFileSync(fullSecretsName, 'utf-8')) as Partial<T>
+    }
+
+    this.config = JSON.parse(fs.readFileSync(fullConfigName, 'utf-8')) as Partial<T>
+  }
+}
